28bd5e0d69f44f04ba30cbf2e019e1cefa7b7aeb
Udi Wieder <udi.wieder@gmail.com>
2016-10-26 12:57:08 -0700

    second attempt on formatting and testing via lambdas

--- src/test/java/org/hiero/sketch/ColumnPerfTest.java
+++ src/test/java/org/hiero/sketch/ColumnPerfTest.java
@@ -5,46 +5,36 @@ import org.hiero.sketch.table.api.ContentsKind;
 import org.hiero.sketch.table.IntArrayColumn;
 import org.junit.Test;
 
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class ColumnPerfTest {
     @Test
-    public void test() {
+    public void testColumnGetInt() {
         IntArrayColumn col;
         final int size = 1000000;
         final int testnum = 100;
         int temp = 0;
-
-        long[] results = new long[testnum];
-        long[] Cresults = new long[testnum];
-
         ColumnDescription desc = new ColumnDescription("test", ContentsKind.Int, false);
         col = new IntArrayColumn(desc, size);
         for (int i=0; i < size; i++)
             col.set(i, i);
-        for (int j=0; j<testnum; j++) {
-            long startTime = System.nanoTime();
+        System.out.println("Running function call test using lambdas");
+        Consumer<Integer>  fcall =  tmp -> {
             for (int i = 0; i < size; i++)
-                temp += col.getInt(i);
-            long endTime = System.nanoTime();
-            results[j] = endTime - startTime;
-        }
-
-        int temp1 = 0;
+                tmp += col.getInt(i);
+        };
+        TestUtil.runPerfTest(fcall,testnum);
         int Rcol[] = new int[size];
         for (int i=0; i < size; i++)
             Rcol[i]=i;
-        for (int j=0; j<testnum; j++) {
-            long startTime = System.nanoTime();
+        Consumer<Integer> dcall = tmp -> {
             for (int i = 0; i < size; i++)
-                temp1 += Rcol[i];
-            long endTime = System.nanoTime();
-            Cresults[j] = endTime - startTime;
-        }
-        assertEquals(temp, temp1);
-        System.out.println(temp);
-        System.out.println("Comparing function call to Direct manimulation of Array");
-        TestUtil.Percentiles(results);
-        TestUtil.Percentiles(Cresults);
+                tmp += Rcol[i];
+        };
+        System.out.println("running test on direct call using lambdas");
+        TestUtil.runPerfTest(dcall, testnum);
     }
 }
