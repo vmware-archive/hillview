bdd810f029a1780261170c60cb87c5b5f81383b4
parikg <parik.g@gmail.com>
2016-11-13 11:01:14 -0800

    Change to TreeVsHeap

--- src/test/java/org/hiero/sketch/TreeVsHeapTest.java
+++ src/test/java/org/hiero/sketch/TreeVsHeapTest.java
@@ -12,12 +12,12 @@ public class TreeVsHeapTest {
 
     @Test
     public void TreeVsHeapOne() {
-        int runs = 5;
+        final int runs = 5;
         for (int i = 1; i < runs; i++) {
             for (int j = 1; j < this.inpSize; j++) {
                 this.randInp[j] = this.rn.nextInt(this.inpSize);
             }
-            int maxSize = 100;
+            final int maxSize = 100;
             final HeapTopK<Integer> myHeap = new HeapTopK<>(maxSize, MyCompare.instance);
             final TreeTopK<Integer> myTree = new TreeTopK<>(maxSize, MyCompare.instance);
             HeapInsertTimed.runOnInp(myHeap, this.randInp);
