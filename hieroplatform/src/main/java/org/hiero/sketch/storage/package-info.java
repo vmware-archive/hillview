bd969266acf2c6ba6dc22af54bbb119d1e728f66
lalithsuresh <suresh.lalith@gmail.com>
2017-02-16 09:23:36 -0800

    All fields, methods, and parameters are now Nonnull by default.

---
+++ hieroplatform/src/main/java/org/hiero/sketch/storage/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Package that doesn't allow null values as method parameters.
+ */
+
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+@MethodsAreNonnullByDefault
+package org.hiero.sketch.storage;
+
+import org.hiero.utils.FieldsAreNonnullByDefault;
+import org.hiero.utils.MethodsAreNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ hieroplatform/src/main/java/org/hiero/utils/MethodsAreNonnullByDefault.java
@@ -0,0 +1,20 @@
+package org.hiero.utils;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+/**
+ * Applies the {@link Nonnull} annotation to every method unless overridden.
+ */
+@Documented
+@Nonnull
+@TypeQualifierDefault(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodsAreNonnullByDefault
+{
+}
