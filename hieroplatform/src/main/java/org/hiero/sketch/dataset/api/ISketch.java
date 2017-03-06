31072e3f3e92342285fcd2c4815043fbed419dd1
mbudiu-vmw <mbudiu@vmware.com>
2017-03-06 13:16:15 -0800

    Updated copyright in hieroweb project

--- hieroplatform/src/main/java/org/hiero/sketch/dataset/api/ISketch.java
+++ hieroplatform/src/main/java/org/hiero/sketch/dataset/api/ISketch.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017 VMware Inc. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0 2
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
