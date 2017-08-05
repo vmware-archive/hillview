8902fe38960451e23d3599643c9376fe74d383b4
Mihai Budiu <mbudiu@vmware.com>
2017-08-04 17:20:12 -0700

    Filter improvements (#138)
    * Improvements in dialogs

--- web/src/main/webapp/dialog.ts
+++ web/src/main/webapp/dialog.ts
@@ -1,100 +1,128 @@
 import {IHtmlElement} from "./ui"
-import {ContentsKind, ColumnDescription} from "./table"
-import {Pair} from "./util"
+import {ContentsKind} from "./table"
 
 // Represents a field in the dialog. It is just the HTML element, and an optional type.
 export class DialogField {
-	html: HTMLSelectElement | HTMLInputElement;
-	type?: ContentsKind;
+    html: HTMLSelectElement | HTMLInputElement;
+    type?: ContentsKind;
 }
 
-// Class that can be extended for making dialogs.
-export abstract class Dialog implements IHtmlElement {
-	protected container: HTMLDivElement;
+// Base class for dialog implementations
+export class Dialog implements IHtmlElement {
+    private container: HTMLDivElement;
+    public onConfirm: () => void;  // method to be invoked when dialog is closed successfully
+    // Stores the input elements and (optionally) their types.
+    private fields: {[fieldName: string]: DialogField} = {};
+    private confirmButton: HTMLButtonElement;
 
-	// Stores the input elements and (optionally) their types.
-	protected fields: {[fieldName: string]: DialogField} = {};
+    // Create a dialog with the given name.
+    constructor(title: string,) {
+        this.onConfirm = null;
+        this.container = document.createElement("div");
+        this.container.classList.add('dialog');
 
-	// Create a dialog with the given name.
-	constructor(title: string) {
-		this.container = document.createElement("div");
-		this.container.classList.add('dialog');
-		document.body.appendChild(this.container);
+        let titleElement = document.createElement("h1");
+        titleElement.textContent = title;
+        this.container.appendChild(titleElement);
 
-		let titleElement = document.createElement("h1");
-		titleElement.textContent = title;
-		this.container.appendChild(titleElement); 
+        let buttonsDiv = document.createElement("div");
+        this.container.appendChild(buttonsDiv);
+        buttonsDiv.classList.add('cancelconfirm');
 
-		let buttonsDiv = document.createElement("div");
-		this.container.appendChild(buttonsDiv);
-		buttonsDiv.classList.add('cancelconfirm');
+        let cancelButton = document.createElement("button");
+        cancelButton.onclick = () => this.cancelAction();
+        cancelButton.textContent = "Cancel";
+        cancelButton.classList.add("cancel");
+        buttonsDiv.appendChild(cancelButton);
 
-		let cancelButton = document.createElement("button");
-		cancelButton.onclick = () => this.cancelAction();
-		cancelButton.textContent = "Cancel";
-		cancelButton.classList.add("cancel");
-		buttonsDiv.appendChild(cancelButton);
+        this.confirmButton = document.createElement("button");
+        this.confirmButton.textContent = "Confirm";
+        this.confirmButton.classList.add("confirm");
+        buttonsDiv.appendChild(this.confirmButton);
+    }
 
-		let confirmButton = document.createElement("button");
-		confirmButton.onclick = () => this.confirmAction();
-		confirmButton.textContent = "Confirm";
-		confirmButton.classList.add("confirm");
-		buttonsDiv.appendChild(confirmButton);
-	}
+    public setAction(onConfirm: () => void): void {
+        this.onConfirm = onConfirm;
+        if (onConfirm != null)
+            this.confirmButton.onclick = () => {
+                this.hide();
+                this.onConfirm();
+            };
+    }
 
-	public getHTMLRepresentation(): HTMLDivElement {
-		return this.container;
-	}
+    // display the menu
+    public show(): void {
+        document.body.appendChild(this.container);
+    }
 
-	// Add a text field with the given internal name, label, and data type.
-	// @param fieldName: Internal name. Has to be used when parsing the input.
-	// @param labelText: Text in the dialog for this field.
-	// @param type: Data type of this field. For now, only Integer is special.
-	protected addTextField(fieldName: string, labelText: string, type: ContentsKind): void {
-		let fieldDiv = document.createElement("div");
-		this.container.appendChild(fieldDiv);
+    // Removes the menu from the DOM
+    public hide(): void {
+        this.container.remove();
+    }
 
-		let label = document.createElement("label");
-		label.textContent = labelText;
-		fieldDiv.appendChild(label);
+    public getHTMLRepresentation(): HTMLDivElement {
+        return this.container;
+    }
 
-		let input = <HTMLInputElement> document.createElement("input");
-		fieldDiv.appendChild(input);
-		if (type == "Integer") {
-			input.type = "number";
-		}
-		this.fields[fieldName] = {html: input, type: type};
-	}
+    // Add a text field with the given internal name, label, and data type.
+    // @param fieldName: Internal name. Has to be used when parsing the input.
+    // @param labelText: Text in the dialog for this field.
+    // @param type: Data type of this field. For now, only Integer is special.
+    public addTextField(fieldName: string, labelText: string, type: ContentsKind): void {
+        let fieldDiv = document.createElement("div");
+        this.container.appendChild(fieldDiv);
 
-	// Add a selection field with the given options.
-	// @param fieldName: Internal name. Has to be used when parsing the input.
-	// @param labelText: Text in the dialog for this field.
-	// @param options: List of strings that are the options in the selection box.
-	protected addSelectField(fieldName: string, labelText: string, options: string[]): void {
-		let fieldDiv = document.createElement("div");
-		this.container.appendChild(fieldDiv);
+        let label = document.createElement("label");
+        label.textContent = labelText;
+        fieldDiv.appendChild(label);
 
-		let label = document.createElement("label");
-		label.textContent = labelText;
-		fieldDiv.appendChild(label);
+        let input = <HTMLInputElement> document.createElement("input");
+        fieldDiv.appendChild(input);
+        if (type == "Integer") {
+            input.type = "number";
+        }
+        this.fields[fieldName] = {html: input, type: type};
+    }
 
-		let select = document.createElement("select");
-		fieldDiv.appendChild(select);
-		options.forEach(option => {
-			let optionElement = document.createElement("option");
-			optionElement.value = option;
-			optionElement.text = option;
-			select.add(optionElement);
-		})
-		this.fields[fieldName] = {html: select}
-	}
+    // Add a selection field with the given options.
+    // @param fieldName: Internal name. Has to be used when parsing the input.
+    // @param labelText: Text in the dialog for this field.
+    // @param options: List of strings that are the options in the selection box.
+    protected addSelectField(fieldName: string, labelText: string, options: string[]): void {
+        let fieldDiv = document.createElement("div");
+        this.container.appendChild(fieldDiv);
 
-	// Remove this element from the DOM.
-	private cancelAction(): void {
-		this.container.remove();
-	}
+        let label = document.createElement("label");
+        label.textContent = labelText;
+        fieldDiv.appendChild(label);
 
-	// Has to be overridden, and should read the input from the 'this.fields' dict, in addition 
-	// to the action that should be done with the input.
-	protected abstract confirmAction(): void;
+        let select = document.createElement("select");
+        fieldDiv.appendChild(select);
+        options.forEach(option => {
+            let optionElement = document.createElement("option");
+            optionElement.value = option;
+            optionElement.text = option;
+            select.add(optionElement);
+        });
+        this.fields[fieldName] = {html: select}
+    }
+
+    public getFieldValue(field: string): string {
+        return this.fields[field].html.value;
+    }
+
+    public getFieldValueAsInt(field: string): number {
+        let s = this.getFieldValue(field);
+        return parseInt(s);
+    }
+
+    public getFieldValueAsNumber(field: string): number {
+        let s = this.getFieldValue(field);
+        return parseFloat(s);
+    }
+
+    // Remove this element from the DOM.
+    private cancelAction(): void {
+        this.hide();
+    }
 }
