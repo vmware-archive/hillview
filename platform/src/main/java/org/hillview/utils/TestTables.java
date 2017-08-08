4d51b8023e378579373cade6367a445a1ad26c3a
Han Kruiger <hankruiger@gmail.com>
2017-08-08 12:58:25 -0700

    More reasonable testing.

--- platform/src/main/java/org/hillview/utils/TestTables.java
+++ platform/src/main/java/org/hillview/utils/TestTables.java
@@ -26,6 +26,7 @@ import org.hillview.table.api.ContentsKind;
 import org.hillview.table.api.IColumn;
 import org.hillview.table.api.IMembershipSet;
 import org.hillview.table.api.ITable;
+import org.jblas.DoubleMatrix;
 import org.junit.Assert;
 
 import java.util.*;
@@ -220,6 +221,36 @@ public class TestTables {
         final List<IColumn> col = new ArrayList<IColumn>();
         col.addAll(Arrays.asList(intCol).subList(0, numCols));
         return new SmallTable(col);
+    }
+
+    /**
+     * @param size Number of rows in the table.
+     * @param numCols Number of columns in the table. Has to be >= 2.
+     * @return A table where the 2nd column is a linear function of the 1st (with some noise). The rest of the columns
+     * contains just small noise from a Gaussian distribution.
+     */
+    public static ITable getLinearTable(final int size, final int numCols) {
+        Random rnd = new Random(42);
+        double noise = 0.01;
+        double a = 1.2;
+
+        DoubleMatrix mat = new DoubleMatrix(size, numCols);
+        for (int i = 0; i < size; i++) {
+            // Make the first two columns linearly correlated.
+            double x = rnd.nextDouble();
+            mat.put(i, 0, x);
+            double y = a * x + rnd.nextGaussian() * noise;
+            mat.put(i, 1, y);
+
+            // Fill the rest of the columns with noise.
+            for (int j = 2; j < numCols; j++) {
+                double z = noise * rnd.nextGaussian();
+                mat.put(i, j, z);
+            }
+        }
+
+        ITable table = BlasConversions.toTable(mat);
+        return table;
     }
 
     /**
--- platform/src/test/java/org/hillview/sketch/CorrelationTest.java
+++ platform/src/test/java/org/hillview/sketch/CorrelationTest.java
@@ -1,12 +1,17 @@
 package org.hillview.sketch;
 
+import org.hillview.dataset.api.IDataSet;
 import org.hillview.sketches.BasicColStatSketch;
 import org.hillview.sketches.BasicColStats;
 import org.hillview.sketches.CorrMatrix;
 import org.hillview.sketches.FullCorrelationSketch;
 import org.hillview.table.api.IColumn;
 import org.hillview.table.api.ITable;
+import org.hillview.utils.BlasConversions;
+import org.hillview.utils.LinAlg;
 import org.hillview.utils.TestTables;
+import org.jblas.DoubleMatrix;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.*;
@@ -14,7 +19,8 @@ import java.util.*;
 public class CorrelationTest {
     @Test
     public void testCorrelation() {
-        ITable table = TestTables.getCorrelatedCols(300, 30, 10);
+        DoubleMatrix mat = new DoubleMatrix(new double[][]{{9, 8, 4, 1, 6}, {5, 8, 2, 10, 1}, {6, 4, 1, 6, 5}});
+        ITable table = BlasConversions.toTable(mat);
         List<String> colNames = new ArrayList<String>(table.getSchema().getColumnNames());
         Map<String, BasicColStats> bcsMap = new HashMap<String, BasicColStats>(table.getSchema().getColumnCount());
         for (IColumn col : table.getColumns()) {
@@ -24,7 +30,46 @@ public class CorrelationTest {
         }
 
         FullCorrelationSketch fcs = new FullCorrelationSketch(colNames, bcsMap);
-        CorrMatrix corrMatrix = fcs.create(table);
-        System.out.println(Arrays.deepToString(corrMatrix.getCorrelationMatrix()));
+        CorrMatrix cm = fcs.create(table);
+
+        DoubleMatrix corrMatrix = new DoubleMatrix(cm.getCorrelationMatrix());
+        DoubleMatrix eigenVectors = LinAlg.eigenVectors(corrMatrix, 2);
+    }
+
+    @Test
+    public void testBigCorrelation() {
+        ITable table = TestTables.getLinearTable(10000, 30);
+        List<String> colNames = new ArrayList<String>(table.getSchema().getColumnNames());
+
+        IDataSet<ITable> dataset = TestTables.makeParallel(table, 1000);
+
+        Map<String, BasicColStats> bcsMap = new HashMap<String, BasicColStats>(table.getSchema().getColumnCount());
+        for (IColumn col : table.getColumns()) {
+            BasicColStatSketch statSketch = new BasicColStatSketch(col.getName(), null);
+            BasicColStats bcs = dataset.blockingSketch(statSketch);
+            bcsMap.put(col.getName(), bcs);
+        }
+
+        FullCorrelationSketch fcs = new FullCorrelationSketch(colNames, bcsMap);
+        CorrMatrix cm = dataset.blockingSketch(fcs);
+
+        DoubleMatrix corrMatrix = new DoubleMatrix(cm.getCorrelationMatrix());
+        // Get just the eigenvector corresponding to the largest eigenvalue (because we know the data is approximately
+        // linear).
+        DoubleMatrix eigenVectors = LinAlg.eigenVectors(corrMatrix, 1);
+        eigenVectors.print();
+
+        for (int i = 2; i < eigenVectors.columns; i++) {
+            // The eigenvector should have reasonably large components in the first two columns, compared to the
+            // other components in the eigenvector.
+            Assert.assertTrue(
+                    "First component of eigenvector not large enough.",
+                    Math.abs(eigenVectors.get(0, 0)) > 3 * Math.abs(eigenVectors.get(0, i))
+            );
+            Assert.assertTrue(
+                    "Second component of eigenvector not large enough.",
+                    Math.abs(eigenVectors.get(0, 1)) > 3 * Math.abs(eigenVectors.get(0, i))
+            );
+        }
     }
 }
