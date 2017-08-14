3355b0abd5fe0c0aa2e215cf059d07f105bdce9e
Han Kruiger <hankruiger@gmail.com>
2017-08-14 11:30:31 -0700

    Small readability changes

--- data/convert_to_csv.py
+++ data/convert_to_csv.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 # convert function by: http://pjreddie.com/projects/mnist-in-csv/
 
+
 def convert(imgf, labelf, outf, n):
     f = open(imgf, "rb")
     o = open(outf, "w")
-    l = open(labelf, "rb")
+    label = open(labelf, "rb")
 
     f.read(16)
-    l.read(8)
+    label.read(8)
     images = []
 
     for i in range(n):
-        image = [ord(l.read(1))]
-        for j in range(28*28):
+        image = [ord(label.read(1))]
+        for j in range(2 * 28):
             image.append(ord(f.read(1)))
         images.append(image)
 
     for image in images:
-        o.write(",".join(str(pix) for pix in image)+"\n")
+        o.write(",".join(str(pix) for pix in image) + "\n")
     f.close()
     o.close()
-    l.close()
+    label.close()
+
 
 if __name__ == "__main__":
     # Convert the data
