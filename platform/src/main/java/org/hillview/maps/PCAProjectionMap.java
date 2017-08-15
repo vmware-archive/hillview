102193ed7558f1f1b5f6d6e105a39d2e4e99567d
Han Kruiger <hankruiger@gmail.com>
2017-08-15 15:54:08 -0700

    Returned to using BLAS for the linear algebra, because it's a lot faster. Also added a test for MNIST.

--- platform/src/main/java/org/hillview/maps/PCAProjectionMap.java
+++ platform/src/main/java/org/hillview/maps/PCAProjectionMap.java
@@ -1,5 +1,6 @@
 package org.hillview.maps;
 
+import org.hillview.dataset.api.IJson;
 import org.hillview.dataset.api.IMap;
 import org.hillview.sketches.CorrMatrix;
 import org.hillview.sketches.FullCorrelationSketch;
@@ -7,30 +8,30 @@ import org.hillview.table.api.ITable;
 import org.hillview.utils.LinAlg;
 import org.jblas.DoubleMatrix;
 
-import java.util.Arrays;
+import java.io.Serializable;
 import java.util.List;
-import java.util.logging.Logger;
 
-public class PCAProjectionMap implements IMap<ITable, ITable> {
-    private static final Logger LOG = Logger.getLogger(PCAProjectionMap.class.getName());
+/**
+ * This map computes the {numProjections} principal components of the data specified by the column names.
+ * Subsequently, the same data is projected along those principal components.
+ * The result is the concatenation of the original table and the result.
+ */
+public class PCAProjectionMap implements IMap<ITable, ITable>, Serializable, IJson {
     private final int numProjections;
     private final List<String> colNames;
 
-    public PCAProjectionMap(String[] colNames, int numProjections) {
-        this.colNames = Arrays.asList(colNames);
+    public PCAProjectionMap(List<String> colNames, int numProjections) {
+        this.colNames = colNames;
         this.numProjections = numProjections;
     }
 
     @Override
     public ITable apply(ITable table) {
         FullCorrelationSketch fcs = new FullCorrelationSketch(this.colNames);
-        LOG.info("Computing correlation matrix");
         CorrMatrix corrMatrix = fcs.create(table);
         DoubleMatrix cm = new DoubleMatrix(corrMatrix.getCorrelationMatrix());
-        LOG.info("Computing eigenvalues");
         DoubleMatrix eigs = LinAlg.eigenVectors(cm, this.numProjections);
         LinearProjectionMap lpm = new LinearProjectionMap(this.colNames, eigs, "PCA", null);
-        LOG.info("Applying projection");
         return lpm.apply(table);
     }
 }
