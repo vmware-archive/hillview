3d874623f30ff4d345312adb3fd7e383b4b16760
Han Kruiger <hankruiger@gmail.com>
2017-08-16 10:12:52 -0700

    Skip test if mnist.csv is not present

--- platform/src/test/java/org/hillview/sketch/PCATest.java
+++ platform/src/test/java/org/hillview/sketch/PCATest.java
@@ -1,11 +1,7 @@
 package org.hillview.sketch;
 
 import org.hillview.dataset.api.IDataSet;
-<<<<<<< HEAD
 import org.hillview.maps.LinearProjectionMap;
-=======
-import org.hillview.maps.PCAProjectionMap;
->>>>>>> Returned to using BLAS for the linear algebra, because it's a lot faster. Also added a test for MNIST.
 import org.hillview.sketches.CorrMatrix;
 import org.hillview.sketches.FullCorrelationSketch;
 import org.hillview.storage.CsvFileReader;
@@ -19,10 +15,7 @@ import org.jblas.DoubleMatrix;
 import org.junit.Assert;
 import org.junit.Test;
 
-<<<<<<< HEAD
 import java.io.FileNotFoundException;
-=======
->>>>>>> Returned to using BLAS for the linear algebra, because it's a lot faster. Also added a test for MNIST.
 import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
