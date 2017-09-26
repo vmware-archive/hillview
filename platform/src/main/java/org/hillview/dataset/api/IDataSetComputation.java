4405f856fee351601ddfc6634f77ef99ae6e5a38
mbudiu-vmw <mbudiu@vmware.com>
2017-09-25 21:27:14 -0700

    Remove empty line in copyright

--- platform/src/main/java/org/hillview/dataset/api/IDataSetComputation.java
+++ platform/src/main/java/org/hillview/dataset/api/IDataSetComputation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 VMware Inc. All Rights Reserved.
+ * Copyright (c) 2017 VMWare Inc. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.hillview.dataset.api;
