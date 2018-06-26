5323ad355eec0ebcc02d206c689d2c24756cc2f7
parikg <parik.g@gmail.com>
2018-06-26 12:26:43 -0700

    *.java

--- platform/src/main/java/org/hillview/sketches/SampleDistinctElementsSketch.java
+++ platform/src/main/java/org/hillview/sketches/SampleDistinctElementsSketch.java
@@ -3,18 +3,16 @@ package org.hillview.sketches;
 import it.unimi.dsi.fastutil.longs.Long2ObjectRBTreeMap;
 import net.openhft.hashing.LongHashFunction;
 import org.hillview.dataset.api.ISketch;
-import org.hillview.table.api.ColumnAndConverterDescription;
-import org.hillview.table.api.IColumn;
-import org.hillview.table.api.IRowIterator;
-import org.hillview.table.api.ITable;
+import org.hillview.table.api.*;
 
 import javax.annotation.Nullable;
+import java.util.Comparator;
 
 /**
  * This sketch produces a uniformly random sample of k distinct values from a column (assuming truly
  * random hashing). The values are sampled from the *set* of distinct values taken by that column.
  */
-public class BottomKSketch implements ISketch<ITable, MinKSet> {
+public class BottomKSketch implements ISketch<ITable, MinKSet<String>> {
     private final String colName;
     LongHashFunction hash;
     final int maxSize;
@@ -32,6 +30,8 @@ public class BottomKSketch implements ISketch<ITable, MinKSet> {
     @Override
     public MinKSet create(ITable data) {
         IColumn col = data.getLoadedColumn(new ColumnAndConverterDescription(this.colName)).column;
+        if (col.getDescription().kind != ContentsKind.String)
+            throw new IllegalArgumentException("BottomKSketch only supports String columns");
         final IRowIterator myIter = data.getMembershipSet().getIterator();
         int currRow = myIter.getNextRow();
         MinKRows mkRows = new MinKRows(this.maxSize);
@@ -41,13 +41,20 @@ public class BottomKSketch implements ISketch<ITable, MinKSet> {
             }
             currRow = myIter.getNextRow();
         }
-        return mkRows.getMinKSet(col);
+        return getMinStrings(col, mkRows);
+    }
+
+    private MinKSet<String> getMinStrings(IColumn col, MinKRows mkRows) {
+        Long2ObjectRBTreeMap<String> data = new Long2ObjectRBTreeMap<String>();
+        for (long hashKey: mkRows.treeMap.keySet())
+            data.put(hashKey, col.getString(mkRows.treeMap.get(hashKey)));
+        return new MinKSet<String>(this.maxSize, data, Comparator.comparing(String::toString));
     }
 
     @Nullable
     @Override
-    public MinKSet zero() {
-        return new MinKSet(maxSize);
+    public MinKSet<String> zero() {
+        return new MinKSet(maxSize, Comparator.<String>naturalOrder());
     }
 
     /**
@@ -55,14 +62,14 @@ public class BottomKSketch implements ISketch<ITable, MinKSet> {
      * k smallest hash values overall.
      */
     @Nullable
-    public MinKSet add(@Nullable MinKSet left, @Nullable MinKSet right) {
-        Long2ObjectRBTreeMap data = new Long2ObjectRBTreeMap();
+    public MinKSet<String> add(@Nullable MinKSet<String> left, @Nullable MinKSet<String> right) {
+        Long2ObjectRBTreeMap<String> data = new Long2ObjectRBTreeMap<>();
         data.putAll(left.data);
         data.putAll(right.data);
         while (data.size() > this.maxSize) {
             long maxKey = data.lastLongKey();
             data.remove(maxKey);
         }
-        return new MinKSet(this.maxSize, data);
+        return new MinKSet(this.maxSize, data, Comparator.<String>naturalOrder());
     }
 }
