649a373bf92ab8f52f7037746343033326aa0e38
parikg <parik.g@gmail.com>
2018-08-24 10:11:04 -0700

    Added headers to Trellis display

--- platform/src/test/java/org/hillview/test/TrellisShapeTest.java
+++ platform/src/test/java/org/hillview/test/TrellisShapeTest.java
@@ -7,16 +7,23 @@ public class TrellisShapeTest {
 
     @Test
     public void tShapeTest() {
+        boolean toPrint = false;
         double[] ratios = {1.25, 1.5, 2};
         for (double ratio : ratios) {
-            ComputeTrellisShape tShape = new ComputeTrellisShape(70, 10, 40, 10, ratio);
-            System.out.printf("\nAspect ratio %f\n", ratio);
-            for (int n = 10; n <= 30; n++) {
+            ComputeTrellisShape tShape = new ComputeTrellisShape(70, 10, 50,
+                    10, ratio, 5);
+            if (toPrint) {
+                System.out.printf("\nAspect ratio %f\n", ratio);
+            }
+            for (int n = 10; n <= 25; n++) {
                 ComputeTrellisShape.TrellisShape optShape = tShape.getShape(n);
-                if (optShape.x_num * optShape.y_num < n)
-                    System.out.printf("Constraints are not feasible for ");
-                System.out.printf("%d: %d * %d, Size %.0f * %.0f, Coverage: %f \n", n, optShape.x_num,
-                        optShape.y_num, optShape.x_len, optShape.y_len, optShape.coverage);
+                if (toPrint) {
+                    if (optShape.x_num * optShape.y_num < n)
+                        System.out.printf("Constraints are not feasible for ");
+                    System.out.printf("%d: %d * %d, Size %.0f * (%.0f + %d), Coverage: %f \n", n, optShape.x_num,
+
+                            optShape.y_num, optShape.x_len, optShape.y_len, 5, optShape.coverage);
+                }
             }
         }
     }
