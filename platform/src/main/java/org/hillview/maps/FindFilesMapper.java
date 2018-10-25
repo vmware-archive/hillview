4e93101fe6e4fee63c58346368ea923db91b93fe
arvind008 <purohith.arvind@gmail.com>
2018-10-25 17:42:39 +0530

    Make the directory optional to exist in all the worker nodes (#474)

--- platform/src/main/java/org/hillview/maps/FindFilesMapper.java
+++ platform/src/main/java/org/hillview/maps/FindFilesMapper.java
@@ -62,9 +62,12 @@ public class FindFilesMapper implements IMap<Empty, List<IFileReference>> {
 
         Stream<Path> files;
         try {
-            files = Files.walk(dir, 1, FileVisitOption.FOLLOW_LINKS);
+            if(Files.exists(dir))
+                files = Files.walk(dir, 1, FileVisitOption.FOLLOW_LINKS);
+            else
+                return Collections.emptyList();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); 
         }
         files = files.filter(f -> {
             if (f == null)
