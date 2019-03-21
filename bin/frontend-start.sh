b1a31f9fb6735671c6d16dc1878d0d186366e3ee
mbudiu-vmw <mbudiu@vmware.com>
2019-03-20 17:18:25 -0700

    Disable grpc logging

--- bin/frontend-start.sh
+++ bin/frontend-start.sh
@@ -10,7 +10,7 @@ source $mydir/lib.sh
 export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 
 # If you want GRPC logging uncomment the following line.
-LOGGING=" -Djava.util.logging.config.file=logging.properties"
+# LOGGING=" -Djava.util.logging.config.file=logging.properties"
 export JAVA_OPTS="$JAVA_OPTS$LOGGING"
 
 cd $mydir/../web  # logs will always be in this folder
