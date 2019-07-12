90dcae247082a3b8d79cd26c657f64bc29ffd9bc
Hui Lyu <huilyu22@gmail.com>
2019-07-12 11:30:09 -0700

    add comments and modify methods in BatchLogAnalysis

--- platform/src/main/java/org/hillview/main/BatchLogAnalysis.java
+++ platform/src/main/java/org/hillview/main/BatchLogAnalysis.java
@@ -22,7 +22,14 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.cli.*;
 
-
+/**
+ * This class is used for generating Timestamp-errorCode heatmaps data files from syslogs of bugs.
+ * The inputs are syslog files of format RFC5424. Rows in syslog files have a field called "errorCode" in the "StructuredData" column.
+ * It extracts value of "errorCode" from "StructuredData" as a new column, to generate heatmaps of "Timestamp" vs. "errorCode".
+ * Each bucket in the heatmap is the count of occurrence of some errorCode in some timestamp interval among syslogs.
+ * The default number of total timestamp intervals for each bug (each heatmap) is 50.
+ * The heatmap data for each bug is saved as a CSV file of columns "Timestamp", "errorCode" and "Count", where each row represents one bucket in the heatmap.
+ */
 public class BatchLogAnalysis {
 
     private static class HeatmapData {
@@ -31,7 +38,16 @@ public class BatchLogAnalysis {
         ArrayList<String> timeLabels = new ArrayList<>();
     }
 
-    private static HeatmapData heatmapErrTime(FileSetDescription desc, LocalDataSet<Empty> local, int numOfTimestampBuckets) {
+    /**
+     * This method generates the HeatmapData of "Timestamp" vs. "errorCode" for a given set of syslogs.
+     * @param desc description of files of RFC5424 log format and specific file names (paths) pattern
+     * @param numOfTimestampBuckets number of timestamp (x-axis) intervals for each heatmap
+     * @return heatmapData including count in each bucket in the heatmap, y-tick-labels errorCodeLabels, and x-tick-labels timeLabels.
+     */
+    private static HeatmapData heatmapErrTime(FileSetDescription desc, int numOfTimestampBuckets) {
+        /* Preprocess file desc and extract "errorCode" from "StructuredData" as a new column */
+        Empty e = new Empty();
+        LocalDataSet<Empty> local = new LocalDataSet<Empty>(e);
         IMap<Empty, List<IFileReference>> finder = new FindFilesMapper(desc);
         IDataSet<IFileReference> found = local.blockingFlatMap(finder);
         IMap<IFileReference, ITable> loader = new LoadFilesMapper();
@@ -39,15 +55,18 @@ public class BatchLogAnalysis {
         ExtractValueFromKeyMap evkm = new ExtractValueFromKeyMap("errorCode", "StructuredData", "errorCode", -1);
         IDataSet<ITable> table1 = table.blockingMap(evkm);
 
+        /* Find Timestamp (x-axis) buckets for the heatmap */
         DoubleDataRangeSketch rangeSketch = new DoubleDataRangeSketch("Timestamp");
         DataRange dataRange = table1.blockingSketch(rangeSketch);
         DoubleHistogramBuckets bucketsTimestamp = new DoubleHistogramBuckets(dataRange.min, dataRange.max, numOfTimestampBuckets);
 
+        /* Find errorCode (y-axis) buckets for the heatmap */
         SampleDistinctElementsSketch sampleSketch = new SampleDistinctElementsSketch("errorCode", 0, 200);
         MinKSet<String> samples = table1.blockingSketch(sampleSketch);
         JsonList<String> leftBoundaries = samples.getLeftBoundaries(200);
         StringHistogramBuckets bucketsErrorCode = new StringHistogramBuckets(leftBoundaries.toArray(new String[0]));
 
+        /* Generate heatmap based on Timestamp buckets and errorCode buckets, and get the count in each bucket */
         HeatmapSketch heatmapSketch = new HeatmapSketch(bucketsTimestamp, bucketsErrorCode, "Timestamp", "errorCode", 1.0, 0);
         Heatmap heatmap = table1.blockingSketch(heatmapSketch);
         HeatmapData heatmapData = new HeatmapData();
@@ -59,8 +78,10 @@ public class BatchLogAnalysis {
                 heatmapData.matrix[i][j] = heatmap.getCount(i, j);
             }
         }
+
+        /* Get y-tick-labels errorCodeLabels and x-tick-labels timeLabels */
         heatmapData.errorCodeLabels = leftBoundaries;
-        for (int x = 0; x < numOfTimestampBuckets; x++){
+        for (int x = 0; x < numOfTimestampBuckets; x++){    // save the start time only of each bucket
             double time = dataRange.min + x * (dataRange.max - dataRange.min) / numOfTimestampBuckets;
             Instant instantTime = Converters.toDate(time);
             String stringDate = Converters.toString(instantTime);
@@ -69,6 +90,12 @@ public class BatchLogAnalysis {
         return heatmapData;
     }
 
+    /**
+     * This method saves the heatmapData as a CSV file.
+     * The file has columns "Timestamp", "errorCode" and "Count", where each row represents one bucket in the heatmap.
+     * @param heatmapData includes count in each bucket, y-tick-labels errorCodeLabels and x-tick-labels timeLabels
+     * @param filepath destination CSV file path
+     */
     private static void saveHeatmapToFile(HeatmapData heatmapData, String filepath) {
         try (PrintWriter writer = new PrintWriter(new File(filepath))) {
             StringBuilder sb = new StringBuilder();
@@ -93,12 +120,20 @@ public class BatchLogAnalysis {
             writer.write(sb.toString());
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
+            System.exit(1);
         }
     }
 
-    public static void getBugHeatmaps(FileSetDescription desc, LocalDataSet<Empty> local, String logDir, String figDir, int numOfTimestampBuckets) {
+    /**
+     * This method generates and saves heatmapData to CSV files given input syslogs.
+     * @param desc description of files of RFC5424 log format
+     * @param logDir source directory of syslogs for all bugs
+     * @param figDir destination directory to save the CSV files for all bugs
+     * @param numOfTimestampBuckets number of timestamp (x-axis) intervals for each heatmap
+     */
+    public static void getBugHeatmaps(FileSetDescription desc, String logDir, String figDir, int numOfTimestampBuckets) {
         File path = new File(logDir);
-        String[] bugIDs = path.list(new FilenameFilter() {
+        String[] bugIDs = path.list(new FilenameFilter() {    // each subDir in logDir corresponds to one bug
             @Override
             public boolean accept(File current, String name) {
                 return new File(current, name).isDirectory();
@@ -112,7 +147,7 @@ public class BatchLogAnalysis {
                     return new File(current, name).isDirectory();
                 }
             });
-            if (subFolders.length != 0) {
+            if (subFolders.length != 0) {      // exists syslogs for that bug
                 desc.fileNamePattern = "";
                 if (subFolders.length == 1) {
                     desc.fileNamePattern = logDir + "/" + bugIDs[i] + "/" + subFolders[0] + "/var/log/syslog*";
@@ -125,8 +160,8 @@ public class BatchLogAnalysis {
                 }
                 String filePathStr = figDir + "/" + "Bug" + bugIDs[i] + ".csv";
                 Path filePath = Paths.get(filePathStr);
-                if (Files.notExists(filePath)){
-                    HeatmapData heatmapData = heatmapErrTime(desc, local, numOfTimestampBuckets);
+                if (Files.notExists(filePath)){    // call the two step methods
+                    HeatmapData heatmapData = heatmapErrTime(desc, numOfTimestampBuckets);
                     saveHeatmapToFile(heatmapData, filePathStr);
                 }
             }
@@ -134,13 +169,12 @@ public class BatchLogAnalysis {
     }
 
     public static void main(String[] args) {
-        Empty e = new Empty();
-        LocalDataSet<Empty> local = new LocalDataSet<Empty>(e);
         FileSetDescription desc = new FileSetDescription();
         desc.fileKind = "genericlog";
         desc.logFormat = "%{RFC5424}";
         desc.headerRow = false;
 
+        /* Arguments parser for two arguments: logDir and figDir */
         Options options = new Options();
         options.addOption("help",false,"java BatchLogAnalysis [-l] logDir [-f] figDir");
         Option l = OptionBuilder.withArgName( "logDir" )
@@ -173,7 +207,7 @@ public class BatchLogAnalysis {
                 if(cmd.hasOption("f")) {
                     figDir = cmd.getOptionValue("f");
                 }
-                getBugHeatmaps(desc, local, logDir, figDir, 50);
+                getBugHeatmaps(desc, logDir, figDir, 50);    // default number of timestamp buckets is 50
             } catch (ParseException err) {
                 System.err.println("java BatchLogAnalysis [-l] logDir [-f] figDir");
                 System.exit(1);
