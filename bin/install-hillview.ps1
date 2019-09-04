956cf3e2714b0a3f815e7bf120d1772191b28851
mbudiu-vmw <mbudiu@vmware.com>
2019-09-03 17:35:14 -0700

    Update powershell script

--- bin/install-hillview.ps1
+++ bin/install-hillview.ps1
@@ -1,7 +1,7 @@
 # This Powershell script installs the software needed to run Hillview on Windows
 
 echo "Installing apache Tomcat web server"
-if ( -not ( Test-Path .\tomcat.zip -PathType Leaf )) {
+if ( -not ( Test-Path .\apache-tomcat-9.0.4 )) {
    Invoke-WebRequest -OutFile tomcat.zip https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.4/bin/apache-tomcat-9.0.4-windows-x64.zip
    Expand-Archive .\tomcat.zip -DestinationPath .
    rm apache-tomcat-9.0.4\webapps\ROOT -r -fo
