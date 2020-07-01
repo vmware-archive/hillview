6a268a9acf58ac823d12acb00723cd72c517424b
Daniar <daniar@MacBook-Pro.local>
2020-07-01 17:51:37 -0500

    adding some tests to check the needed SSTable files, reading SSTable, counting rows, and test lazy loading

--- platform/src/test/java/org/hillview/test/storage/SSTableTest.java
+++ platform/src/test/java/org/hillview/test/storage/SSTableTest.java
@@ -17,24 +17,90 @@
 
 package org.hillview.test.storage;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 import org.hillview.storage.CassandraSSTableLoader;
+import org.hillview.table.api.IColumn;
 import org.hillview.table.api.ITable;
 import org.hillview.test.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SSTableTest extends BaseTest{
+    private String ssTableDir = "../data/sstable/";
+    private String ssTablePath = "../data/sstable/md-2-big-Data.db";
+        
+    @Test
+    public void testSSTableComplimentaryFiles() throws Exception {
+        try{
+            File directoryPath = new File(ssTableDir);
+            String contents[] = directoryPath.list();
+            int counter = 0;
+            for(int i=0; i<contents.length; i++) {
+                if(contents[i].startsWith("md-"))
+                    counter ++;
+            }
+            Assert.assertEquals(8, counter);
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace().toString());
+            // this will fail if SSTable path is not valid, but we don't want to fail the test.
+            this.ignoringException("Failed to read SSTable (" + ssTablePath + ")", e);
+            return;
+        }
+    }
+
     @Test
     public void testReadingSSTable() throws Exception {
-        String ssTablePath = "/Users/daniar/Documents/Github/hillview/data/sstable/md-2-big-Data.db";
-        CassandraSSTableLoader ssTableLoader = new CassandraSSTableLoader(ssTablePath);
+        CassandraSSTableLoader ssTableLoader = new CassandraSSTableLoader(this.ssTablePath, false);
         try{
             ITable table = ssTableLoader.load();
             Assert.assertEquals("Table[4x15]", table.toString());
         } catch (Exception e) {
+            System.out.println(e.getStackTrace().toString());
             // this will fail if SSTable path is not valid, but we don't want to fail the test.
-            this.ignoringException("Failed to read SSTable (" + ssTablePath + ")", e);
+            this.ignoringException("Failed to read SSTable (" + this.ssTablePath + ")", e);
+            return;
         }
     }
-    
+
+    @Test
+    public void testRowCount() throws Exception {
+        CassandraSSTableLoader ssTableLoader = new CassandraSSTableLoader(this.ssTablePath, false);
+        try{
+            int rowCount = ssTableLoader.getNumRows();
+            Assert.assertEquals(15, rowCount);
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace().toString());
+            // this will fail if SSTable path is not valid, but we don't want to fail the test.
+            this.ignoringException("Failed to read SSTable (" + this.ssTablePath + ")", e);
+            return;
+        }
+    }
+
+    @Test
+    public void testLazyLoading() throws Exception {
+        CassandraSSTableLoader ssTableLoader = new CassandraSSTableLoader(this.ssTablePath, true);
+        try{
+            ITable table = ssTableLoader.load();
+            IColumn col = table.getLoadedColumn("name");
+            String firstName = col.getString(0);
+            Assert.assertEquals("susi", firstName);
+
+            List<String> colNames = Arrays.asList("salary","address");
+            List<IColumn> listCols = table.getLoadedColumns(colNames);
+            String address = listCols.get(1).getString(3);
+            int salary = listCols.get(0).getInt(3);
+
+            Assert.assertEquals(2, listCols.size());
+            Assert.assertEquals("Hyderabad", address);
+            Assert.assertEquals(40000, salary);
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace().toString());
+            // this will fail if SSTable path is not valid, but we don't want to fail the test.
+            this.ignoringException("Failed to read SSTable (" + this.ssTablePath + ")", e);
+            return;
+        }
+    }
 }
