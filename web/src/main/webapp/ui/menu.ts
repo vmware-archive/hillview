345f0bba9bf583a2bc7ea65da1583d391f56f20f
Daniar Kurniawan <ddhhkk2@gmail.com>
2020-07-28 13:38:11 -0500

    undo the changes on menu.ts

--- web/src/main/webapp/ui/menu.ts
+++ web/src/main/webapp/ui/menu.ts
@@ -1,4 +1,3 @@
-import { tickStep } from "d3-array";
 /*
  * Copyright (c) 2017 VMware Inc. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
@@ -41,279 +40,157 @@ export interface MenuItem extends BaseMenuItem {
     readonly action: () => void;
 }
 
-interface subMenuCells {
-    cells: HTMLTableDataCellElement[];
-}
-
-function emptySubMenu(): subMenuCells {
-    return {
-        cells: []
-    }
-}
-
 abstract class BaseMenu<MI extends BaseMenuItem> implements IHtmlElement {
-  public items: MI[];
-  /**
-   * Actual html representations corresponding to the submenu items.
-   * They are stored in the same order as the items.
-   */
-  public outer: HTMLTableElement;
-  public tableBody: HTMLTableSectionElement;
+    public items: MI[];
+    /**
+     * Actual html representations corresponding to the submenu items.
+     * They are stored in the same order as the items.
+     */
+    public outer: HTMLTableElement;
+    public tableBody: HTMLTableSectionElement;
+    public cells: HTMLTableDataCellElement[];
+    public selectedIndex: number;  // -1 if no item is selected
 
-  public cells: HTMLTableDataCellElement[];
-  public subMenuCells: subMenuCells[];
-  public rows: HTMLTableRowElement[];
-  public selectedIndex: number; // -1 if no item is selected
-  public selectedParentMenu: number; // -1 if no item is selected
-  public selectedSubMenu: number; // -1 if no item is selected
-
-  protected constructor() {
-    this.items = [];
-    this.cells = [];
-    this.subMenuCells = [];
-    this.selectedParentMenu = -1;
-    this.rows = [];
-    this.selectedIndex = -1;
-    this.outer = document.createElement("table");
-    this.outer.classList.add("menu", "hidden");
-    this.tableBody = this.outer.createTBody();
-    this.outer.onkeydown = (e) => this.keyAction(e);
-  }
-
-  protected addItems(mis: MI[]): void {
-    if (mis != null) {
-      for (const mi of mis) this.addItem(mi, true);
-    }
-  }
-
-  public abstract setAction(mi: MI, enabled: boolean): void;
-
-  public abstract setSubMenuAction(parentIndex: number, subMenuIndex: number,
-        mi: MI, enabled: boolean): void;
-
-  public keyAction(e: KeyboardEvent): void {
-    if (e.code === "ArrowDown" && this.selectedIndex < this.cells.length - 1) {
-      this.select(this.selectedIndex + 1);
-    } else if (e.code === "ArrowUp" && this.selectedIndex > 0) {
-      this.select(this.selectedIndex - 1);
-    } else if (e.code === "Enter" && this.selectedIndex >= 0) {
-      // emulate a mouse click on this cell
-      this.cells[this.selectedIndex].click();
-    } else if (e.code === "Escape") {
-      this.hide();
-    }
-  }
-
-  /**
-   * Find the position of an item based on its text.
-   * @param text   Text string in the item.
-   */
-  public find(text: string): number {
-    for (let i = 0; i < this.items.length; i++)
-      if (this.items[i].text === text) return i;
-    return -1;
-  }
-
-  public getHTMLRepresentation(): HTMLElement {
-    return this.outer;
-  }
-
-  public hide(): void {
-    this.outer.classList.add("hidden");
-  }
-
-  public getCell(mi: MI): HTMLTableCellElement {
-    const index = this.find(mi.text);
-    if (index < 0) throw new Error("Cannot find menu item");
-    return this.cells[index];
-  }
-
-  public expandMenu(parentMenu: string) {
-    const index = this.find(parentMenu);
-    this.selectedParentMenu = index;
-    if (index < 0) throw new Error("Cannot find menu item " + parentMenu);
-    this.subMenuCells[index].cells.forEach((cell) => {
-      cell.style.display = "block";
-    });
-  }
-
-  public addItem(mi: MI, enabled: boolean): HTMLTableDataCellElement {
-    const index = this.items.length;
-    this.items.push(mi);
-    const trow = this.tableBody.insertRow();
-    this.rows.push(trow);
-    const cell = trow.insertCell(0);
-    this.cells.push(cell);
-    if (mi.text === "---") cell.innerHTML = "<hr>";
-    else cell.textContent = mi.text;
-    cell.id = makeId(mi.text);
-    cell.style.textAlign = "left";
-    cell.classList.add("menuItem");
-    if (mi.help != null) cell.title = mi.help;
-    cell.onmouseenter = () => this.select(index);
-    cell.onmouseleave = () => this.select(-1);
-    this.enable(mi.text, enabled);
-    return cell;
-  }
-
-  /**
-   * Inserting subMenu must be done in the end of all parent menu insert. If the
-   * submenu exceed the length of all main menu, we need to add dummyMenu
-   * */
-  public insertSubMenu(parentText: string, mi: MI, enabled: boolean): void {
-    const parentIndex = this.find(parentText);
-    if (parentIndex < 0) throw new Error("Cannot find menu item " + parentText);
-
-    if (this.subMenuCells[parentIndex] === undefined) {
-      this.subMenuCells[parentIndex] = emptySubMenu();
+    protected constructor() {
+        this.items = [];
+        this.cells = [];
+        this.selectedIndex = -1;
+        this.outer = document.createElement("table");
+        this.outer.classList.add("menu", "hidden");
+        this.tableBody = this.outer.createTBody();
+        this.outer.onkeydown = (e) => this.keyAction(e);
     }
 
-    const subMenuIndex = this.subMenuCells[parentIndex].cells.length;
-    const subMenuPlacementIdx = parentIndex + subMenuIndex;
-    if (this.rows[subMenuPlacementIdx] === undefined) {
-      const trow = this.tableBody.insertRow();
-      this.rows.push(trow);
-      const cell = trow.insertCell(0);
-      cell.classList.add("dummyMenu");
-    }
-    const cell = this.rows[subMenuPlacementIdx].insertCell(1);
-    cell.id = makeId(mi.text);
-    cell.style.textAlign = "left";
-    cell.style.display = "none";
-    cell.classList.add("menuItem");
-    if (enabled) cell.classList.remove("disabled");
-    else cell.classList.add("disabled");
-    if (mi.help != null) cell.title = mi.help;
-    if (mi.text === "---") cell.innerHTML = "<hr>";
-    else cell.textContent = mi.text;
-
-    var subIndex = this.subMenuCells[parentIndex].cells.length;
-    this.subMenuCells[parentIndex].cells.push(cell);
-    cell.onmouseenter = () => this.selectSubMenu(parentIndex, subIndex);
-    cell.onmouseleave = () => this.selectSubMenu(parentIndex, -1);
-    this.setSubMenuAction(parentIndex, subMenuIndex, mi, enabled);
-  }
-
-  public addExpandableItem(mi: MI, enabled: boolean): HTMLTableDataCellElement {
-    const cell = this.addItem(mi, enabled);
-    cell.classList.add("expandableMenu");
-
-    const arrow = document.createElement("span");
-    arrow.textContent = "▸";
-    arrow.style.float = "right";
-    cell.appendChild(arrow);
-
-    // reset the action so that it doesn't hide() after click
-    this.enable(mi.text, enabled);
-    return cell;
-  }
-
-  /**
-   * Highlight a menu item (based on mouse position on keyboard actions).
-   * @param {number} index      Index of item to highlight.
-   * @param {boolean} selected  True if the item is being selected.
-   */
-  public markSelect(index: number, selected: boolean): void {
-    if (index >= 0 && index < this.cells.length) {
-      const cell = this.cells[index];
-      if (selected) {
-        // remove submenu if the parentMenu is not selected
-        if (this.selectedParentMenu != -1 && index != this.selectedParentMenu) {
-          this.hideAllSubMenu();
+    protected addItems(mis: MI[]): void {
+        if (mis != null) {
+            for (const mi of mis)
+                this.addItem(mi, true);
         }
-        // Show subMenu when parent menu is active
-        if (cell.classList.contains("expandableMenu")) {
-          this.expandMenu(cell.firstChild.textContent);
+    }
+
+    public abstract setAction(mi: MI, enabled: boolean): void;
+
+    public keyAction(e: KeyboardEvent): void {
+        if (e.code === "ArrowDown" && this.selectedIndex < this.cells.length - 1) {
+            this.select(this.selectedIndex + 1);
+        } else if (e.code === "ArrowUp"  && this.selectedIndex > 0) {
+            this.select(this.selectedIndex - 1);
+        } else if (e.code === "Enter" && this.selectedIndex >= 0) {
+            // emulate a mouse click on this cell
+            this.cells[this.selectedIndex].click();
+        } else if (e.code === "Escape") {
+            this.hide();
         }
-        cell.classList.add("selected");
-        this.outer.focus();
-      } else {
-        cell.classList.remove("selected");
-      }
     }
-  }
 
-  public select(index: number): void {
-    if (this.selectedIndex >= 0) this.markSelect(this.selectedIndex, false);
-    if (index < 0 || index >= this.cells.length) index = -1; // no one
-    this.selectedIndex = index;
-    this.markSelect(this.selectedIndex, true);
-  }
-
-  public enableByIndex(index: number, enabled: boolean): void {
-    const cell = this.cells[index];
-    if (enabled) cell.classList.remove("disabled");
-    else cell.classList.add("disabled");
-    this.setAction(this.items[index], enabled);
-  }
-
-  public hideAllSubMenu(): void {
-    this.subMenuCells[this.selectedParentMenu].cells.forEach((cell) => {
-      cell.style.display = "none";
-      cell.classList.remove("selected");
-    });
-    this.markSelect(this.selectedParentMenu, false);
-    this.selectedParentMenu = -1;
-    this.selectedSubMenu = -1;
-  }
-
-  public markSelectSubMenu(index: number, selected: boolean): void {
-    if (
-      index >= 0 &&
-      index < this.subMenuCells[this.selectedParentMenu].cells.length
-    ) {
-      const cell = this.subMenuCells[this.selectedParentMenu].cells[index];
-      if (selected) {
-        cell.classList.add("selected");
-        this.outer.focus();
-      } else {
-        cell.classList.remove("selected");
-      }
+    /**
+     * Find the position of an item based on its text.
+     * @param text   Text string in the item.
+     */
+    public find(text: string): number {
+        for (let i = 0; i < this.items.length; i++)
+            if (this.items[i].text === text)
+                return i;
+        return -1;
     }
-  }
 
-  public selectSubMenu(parentIndex: number, subIndex: number): void {
-    if (subIndex == -1) {
-      this.markSelectSubMenu(this.selectedSubMenu, false);
-    } else {
-      this.markSelect(parentIndex, true);
-      if (
-        subIndex < 0 ||
-        subIndex >= this.subMenuCells[parentIndex].cells.length
-      )
-        subIndex = -1; // no one
-      this.selectedSubMenu = subIndex;
-      this.markSelectSubMenu(this.selectedSubMenu, true);
+    public getHTMLRepresentation(): HTMLElement {
+        return this.outer;
     }
-  }
 
-  public enableItem(mi: MI, enabled: boolean): void {
-    this.enable(mi.text, enabled);
-  }
+    public hide(): void {
+        this.outer.classList.add("hidden");
+    }
 
-  /**
-   * Remove all elements from the menu.
-   */
-  public clear(): void {
-    this.tableBody.remove();
-    this.items = [];
-    this.cells = [];
-    this.rows = [];
-    this.subMenuCells = [];
-    this.tableBody = this.outer.createTBody();
-  }
+    public getCell(mi: MI): HTMLTableCellElement {
+        const index = this.find(mi.text);
+        if (index < 0)
+            throw new Error("Cannot find menu item");
+        return this.cells[index];
+    }
 
-  /**
-   * Mark a menu item as enabled or disabled.  If disabled the action
-   * cannot be triggered.
-   * @param {string} text      Text of the item which identifies the item.
-   * @param {boolean} enabled  If true the menu item is enabled, else it is disabled.
-   */
-  public enable(text: string, enabled: boolean): void {
-    const index = this.find(text);
-    if (index < 0) throw new Error("Cannot find menu item " + text);
-    this.enableByIndex(index, enabled);
-  }
+    public addItem(mi: MI, enabled: boolean): HTMLTableDataCellElement {
+        const index = this.items.length;
+        this.items.push(mi);
+        const trow = this.tableBody.insertRow();
+        const cell = trow.insertCell(0);
+        this.cells.push(cell);
+        if (mi.text === "---")
+            cell.innerHTML = "<hr>";
+        else
+            cell.textContent = mi.text;
+        cell.id = makeId(mi.text);
+        cell.style.textAlign = "left";
+        if (mi.help != null)
+            cell.title = mi.help;
+        cell.classList.add("menuItem");
+        cell.onmouseenter = () => this.select(index);
+        cell.onmouseleave = () => this.select(-1);
+        this.enable(mi.text, enabled);
+        return cell;
+    }
+
+    /**
+     * Highlight a menu item (based on mouse position on keyboard actions).
+     * @param {number} index      Index of item to highlight.
+     * @param {boolean} selected  True if the item is being selected.
+     */
+    public markSelect(index: number, selected: boolean): void {
+        if (index >= 0 && index < this.cells.length) {
+            const cell = this.cells[index];
+            if (selected) {
+                cell.classList.add("selected");
+                this.outer.focus();
+            } else {
+                cell.classList.remove("selected");
+            }
+        }
+    }
+
+    public select(index: number): void {
+        if (this.selectedIndex >= 0)
+            this.markSelect(this.selectedIndex, false);
+        if (index < 0 || index >= this.cells.length)
+            index = -1;  // no one
+        this.selectedIndex = index;
+        this.markSelect(this.selectedIndex, true);
+    }
+
+    public enableByIndex(index: number, enabled: boolean): void {
+        const cell = this.cells[index];
+        if (enabled)
+            cell.classList.remove("disabled");
+        else
+            cell.classList.add("disabled");
+        this.setAction(this.items[index], enabled);
+    }
+
+    public enableItem(mi: MI, enabled: boolean): void {
+        this.enable(mi.text, enabled);
+    }
+
+    /**
+     * Remove all elements from the menu.
+     */
+    public clear(): void {
+        this.tableBody.remove();
+        this.items = [];
+        this.cells = [];
+        this.tableBody = this.outer.createTBody();
+    }
+
+    /**
+     * Mark a menu item as enabled or disabled.  If disabled the action
+     * cannot be triggered.
+     * @param {string} text      Text of the item which identifies the item.
+     * @param {boolean} enabled  If true the menu item is enabled, else it is disabled.
+     */
+    public enable(text: string, enabled: boolean): void {
+        const index = this.find(text);
+        if (index < 0)
+            throw new Error("Cannot find menu item " + text);
+        this.enableByIndex(index, enabled);
+    }
 }
 
 /**
@@ -372,34 +249,10 @@ export class ContextMenu extends BaseMenu<MenuItem> implements IHtmlElement {
     public setAction(mi: MenuItem, enabled: boolean): void {
         const index = this.find(mi.text);
         const cell = this.cells[index];
-        if (mi.action != null && enabled) {
-            if (cell.classList.contains("expandableMenu")) {
-              cell.onclick = () => {
-                // this.hide();
-                mi.action();
-              };
-            } else {
-              cell.onclick = () => {
-                this.hide();
-                mi.action();
-              };
-            }
-        } else {
+        if (mi.action != null && enabled)
+            cell.onclick = () => { this.hide(); mi.action(); };
+        else
             cell.onclick = () => this.hide();
-        }
-    }
-
-    public setSubMenuAction(parentIndex: number, subMenuIndex: number,
-        mi: MenuItem, enabled: boolean): void {
-        const cell = this.subMenuCells[parentIndex].cells[subMenuIndex];
-        if (mi.action != null && enabled) {
-            cell.onclick = () => {
-                this.hide();
-                mi.action();
-            };
-        } else {
-            cell.onclick = () => this.hide();
-        }
     }
 }
 
@@ -428,11 +281,6 @@ export class SubMenu extends BaseMenu<MenuItem> implements IHtmlElement {
             cell.onclick = (e: MouseEvent) => { e.stopPropagation(); this.hide(); mi.action(); };
         else
             cell.onclick = (e: MouseEvent) => { e.stopPropagation(); this.hide(); };
-    }
-
-    public setSubMenuAction(parentIndex: number, subMenuIndex: number,
-        mi: MenuItem, enabled: boolean): void {
-        throw new Error("Method not implemented.");
     }
 }
 
@@ -491,11 +339,6 @@ export class TopMenu extends BaseMenu<TopMenuItem> {
             cell.classList.remove("selected");
             this.hideSubMenus();
         };
-    }
-
-    public setSubMenuAction(parentIndex: number, subMenuIndex: number,
-        mi: TopMenuItem, enabled: boolean): void {
-        throw new Error("Method not implemented.");
     }
 
     public getHTMLRepresentation(): HTMLElement {
